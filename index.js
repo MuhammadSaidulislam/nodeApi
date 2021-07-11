9ec2df821713e8a233afa8763ff09a3db82a916f
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-07-11 23:20:33 +0600

    data pass

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-
-
-
 const express = require('express');
+var cors = require('cors')
 const app = express();
-app.get('/', (req, res) =>{
-    res.send("Thank you for calling me YES!!");
+app.use(cors())
+
+
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
 });
 app.listen(3000, () => console.log("Listening port 3000"));
