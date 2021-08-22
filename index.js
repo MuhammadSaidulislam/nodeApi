3ea0bc9e97ddd82e50c557820a8c140101c5f86f
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-08-22 10:00:50 +0600

    github update

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ app.get('/', (req, res) =>{
     res.send("Thank you for calling me");
 })
 
+
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
+})
+
 app.listen(3000, () => console.log("Listening port 3000"));
