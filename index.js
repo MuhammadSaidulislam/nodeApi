a3a3370165ee9f8d2840196e6928d43da596f815
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-09-25 20:18:58 +0600

    listening port set

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const express = require('express');
-
 const app = express();
-
-app.get('/', (req, res) =>{
-    res.send("Thank you for calling me");
-})
-
+// app.get('/', (req, res) =>{
+//     res.send("Thank you for calling me");
+// });
 const user = ['', 'userOne','userTwo','userthree','userFour']
 app.get('/user/:id', (req,res)=>{
     const id = req.params.id;
     const name = user[id];
     res.send({name,id});
-})
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
-})
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
-})
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
-})
+});
 app.listen(3000, () => console.log("Listening port 3000"));
