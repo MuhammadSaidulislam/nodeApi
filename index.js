3796f175d0ce3b006b59ec48f2f9f39cb075b731
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-09-30 23:27:25 +0600

    express set

--- index.js
+++ index.js
@@ -28,3 +28,33 @@ app.get('/user/:id', (req,res)=>{
     res.send({name,id});
 });
 app.listen(3000, () => console.log("Listening port 3000"));
+const express = require('express');
+const app = express();
+// app.get('/', (req, res) =>{
+//     res.send("Thank you for calling me");
+// });
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
+});
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
+});
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
+});
+const user = ['', 'userOne','userTwo','userthree','userFour']
+app.get('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const name = user[id];
+    res.send({name,id});
+});
+app.listen(3000, () => console.log("Listening port 3000"));
