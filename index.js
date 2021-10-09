332a377452052b434f3fcad16c194ecdc3b5c27a
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-10-09 22:19:13 +0600

    nodedom set

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-// app.get('/', (req, res) =>{
-//     res.send("Thank you for calling me");
-// });
-const user = ['', 'userOne','userTwo','userthree','userFour']
-app.get('/user/:id', (req,res)=>{
-    const id = req.params.id;
-    const name = user[id];
-    res.send({name,id});
+app.get('/', (req, res) =>{
+    res.send("Thank you for calling me YES!!");
 });
 
+
+
 app.listen(3000, () => console.log("Listening port 3000"));
+
