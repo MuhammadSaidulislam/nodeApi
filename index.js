b207ce468e7681b27b3049f524c1bb1258b37336
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-10-16 23:29:25 +0600

    ui set

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ app.get('/', (req, res) =>{
 
 app.listen(3000, () => console.log("Listening port 3000"));
 
+const express = require('express');
+const app = express();
+app.get('/', (req, res) =>{
+    res.send("Thank you for calling me YES!!");
+});
+
+
+
+app.listen(3000, () => console.log("Listening port 3000"));
+
+app.listen(3000, () => console.log("Listening port 3000"));
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) =>{
+    res.send("Thank you for calling me YES!!");
+});
+
+
+
+app.listen(3000, () => console.log("Listening port 3000"));
