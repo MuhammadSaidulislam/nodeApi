17189d9be3aab24a4c55fd549032e3483922f182
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-12-01 10:38:48 +0600

    postman set

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) =>{
-    res.send("Thank you for calling me YES!!");
-});
 
 
-
-app.listen(3000, () => console.log("Listening port 3000"));
 
 const express = require('express');
 const app = express();
 app.get('/', (req, res) =>{
     res.send("Thank you for calling me YES!!");
 });
-
-
-
-app.listen(3000, () => console.log("Listening port 3000"));
-
-app.listen(3000, () => console.log("Listening port 3000"));
-
-const express = require('express');
-const app = express();
-app.get('/', (req, res) =>{
-    res.send("Thank you for calling me YES!!");
-});
-
-
-
 app.listen(3000, () => console.log("Listening port 3000"));
