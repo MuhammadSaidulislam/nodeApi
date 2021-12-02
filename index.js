ec87019fe0ffdddcc35c1da40a8b4a10fbf8c1e2
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-12-02 22:48:57 +0600

    post method add

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 
 
 const user = ['', 'userOne','userTwo','userthree','userFour']
@@ -10,4 +14,10 @@ app.get('/user/:id', (req,res)=>{
     const name = user[id];
     res.send({name,id});
 });
+
+
+// post
+app.post('/adduser',(req,res)=>{
+    console.log(req.body);
+})
 app.listen(3000, () => console.log("Listening port 3000"));
